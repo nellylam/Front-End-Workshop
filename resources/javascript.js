0c5a5687d9f379fc46d28f500f5e30a7e117868e
Nelly Lam <nellyannlam@gmail.com>
2014-07-12 14:44:52 -0700

    Added JQuery to copyright text

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,1 +1,9 @@
-// Your Javascript will go here!
+// $("document").ready(function() {
+// 	$("#copyright").click(function(){
+// 		$(this).css('color','purple');
+// 	});
+// });
+
+document.getElementById('copyright').onclick = function () { 
+   this.style.color = 'purple';
+}
